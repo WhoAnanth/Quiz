d6984131ca6da0f790ffcb36e77dff86205df7f8
USER-ANANTH <ananths2005@gmail.com>
2025-07-07 00:11:30 +0530

    initial commit

---
+++ quiz.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+int main()
+{   
+    int score{0};
+    std::cout << "HELLO, WELCOME TO THE QUIZ!"<< '\n';
+
+    std::cout<<"Starting.\n.\n.\n.\n.\n.\n";
+    
+    std::cout << "QUESTION 1 \nWhat is the capital of India?\n";
+    std::cout << "1) Bengaluru \n2) Chennai \n3) Bombay \n4) New Delhi\n" ;
+    int correctAns{4};
+    int userAns{};
+    std::cout << "Enter option number: ";
+    std::cin >>userAns;
+    if (userAns == correctAns){std::cout<< "correct! 1 point awarded!\n";
+        score++;}
+    
+    else {std::cout <<"Wrong! O points awarded!\n";}
+
+    std::cout<<"Your final score is: "<< score<< '\n';    
+
+
+    return 0;
+}
